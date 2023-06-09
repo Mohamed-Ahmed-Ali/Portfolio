1d5c53e169ed1453028e98d2b0e4042b62b6072f
Mohamed-Ahmed-Ali <124755995+Mohamed-Ahmed-Ali@users.noreply.github.com>
2023-06-09 05:37:19 +0300

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,48 +9,54 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
-    const [width, setWidth] = useState(1200);
-    const [numPages, setNumPages] = useState(null);
+  const [width, setWidth] = useState(1200);
+  const [numPages, setNumPages] = useState(null);
 
-    useEffect(() => {
-        setWidth(window.innerWidth);
-    }, []);
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
 
-    function onDocumentLoadSuccess({ numPages }) {
-        setNumPages(numPages);
-    }
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
 
-    return (
-        <div>
-            <Container fluid className="resume-section">
-                <Particle />
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        <Particle />
 
-                <Row className="resume">
-                    <Document
-                        file={pdf}
-                        className="d-flex justify-content-center"
-                        onLoadSuccess={onDocumentLoadSuccess}
-                    >
-                        {Array.from(new Array(numPages), (el, index) => (
-                            <Page key={`page_${index + 1}`} pageNumber={index + 1} scale={width > 786 ? 1.7 : 0.6} />
-                        ))}
-                    </Document>
-                </Row>
+        <Row className="resume">
+          <Document
+            file={pdf}
+            className="d-flex justify-content-center"
+            onLoadSuccess={onDocumentLoadSuccess}
+          >
+            <div style={{ display: "flex", flexWrap: "wrap" }}>
+              {Array.from(new Array(numPages), (el, index) => (
+                <Page
+                  key={`page_${index + 1}`}
+                  pageNumber={index + 1}
+                  width={width > 786 ? width / numPages : width}
+                />
+              ))}
+            </div>
+          </Document>
+        </Row>
 
-                <Row style={{ justifyContent: "center", position: "relative" }}>
-                    <Button
-                        variant="primary"
-                        href={pdf}
-                        target="_blank"
-                        style={{ maxWidth: "250px" }}
-                    >
-                        <AiOutlineDownload />
-                        &nbsp;Download CV
-                    </Button>
-                </Row>
-            </Container>
-        </div>
-    );
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default ResumeNew;
